1ef03fcad8273e2f0c0b7a07ff1008937c0952fc
Maxim Balashov <mbalashov@diasoft.ru>
2025-10-25 23:58:38 +0300

    Test camunda work

--- bpmn/scripts/start.sh
+++ bpmn/scripts/start.sh
@@ -1,1 +1,4 @@
 curl -X POST http://localhost:8080/engine-rest/process-definition/{id}/start -H "Content-Type: application/json" -d '{}'
+
+
+curl -X POST http://localhost:8080/engine-rest/process-definition/key/your-process-key/start -H "Content-Type: application/json" -d '{}'
